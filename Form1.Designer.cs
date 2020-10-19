55c4aaf2d32cadf0663f9f02b35a060d60513e6d
Areeb <arainareeb@gmail.com>
2020-10-19 17:26:31 +0500

    cleaned up more code [Exactly 50 lines now]

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -77,7 +77,7 @@
             this.btnDivide.TabIndex = 4;
             this.btnDivide.Text = "/";
             this.btnDivide.UseVisualStyleBackColor = true;
-            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            this.btnDivide.Click += new System.EventHandler(this.ArithematicBtnClick);
             // 
             // btnMultiply
             // 
@@ -91,7 +91,7 @@
             this.btnMultiply.TabIndex = 8;
             this.btnMultiply.Text = "*";
             this.btnMultiply.UseVisualStyleBackColor = true;
-            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            this.btnMultiply.Click += new System.EventHandler(this.ArithematicBtnClick);
             // 
             // btn9
             // 
@@ -105,7 +105,7 @@
             this.btn9.TabIndex = 7;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
-            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            this.btn9.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn8
             // 
@@ -119,7 +119,7 @@
             this.btn8.TabIndex = 6;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
-            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            this.btn8.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn7
             // 
@@ -133,7 +133,7 @@
             this.btn7.TabIndex = 5;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
-            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            this.btn7.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btnMinus
             // 
@@ -147,7 +147,7 @@
             this.btnMinus.TabIndex = 12;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
-            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            this.btnMinus.Click += new System.EventHandler(this.ArithematicBtnClick);
             // 
             // btn6
             // 
@@ -161,7 +161,7 @@
             this.btn6.TabIndex = 11;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
-            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            this.btn6.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn5
             // 
@@ -175,7 +175,7 @@
             this.btn5.TabIndex = 10;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
-            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            this.btn5.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn4
             // 
@@ -189,7 +189,7 @@
             this.btn4.TabIndex = 9;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
-            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            this.btn4.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btnPlus
             // 
@@ -203,7 +203,7 @@
             this.btnPlus.TabIndex = 16;
             this.btnPlus.Text = "+";
             this.btnPlus.UseVisualStyleBackColor = true;
-            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            this.btnPlus.Click += new System.EventHandler(this.ArithematicBtnClick);
             // 
             // btn3
             // 
@@ -217,7 +217,7 @@
             this.btn3.TabIndex = 15;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = true;
-            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            this.btn3.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn2
             // 
@@ -231,7 +231,7 @@
             this.btn2.TabIndex = 14;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = true;
-            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            this.btn2.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn1
             // 
@@ -245,7 +245,7 @@
             this.btn1.TabIndex = 13;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = true;
-            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            this.btn1.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btnDecimal
             // 
@@ -259,7 +259,7 @@
             this.btnDecimal.TabIndex = 19;
             this.btnDecimal.Text = ".";
             this.btnDecimal.UseVisualStyleBackColor = true;
-            this.btnDecimal.Click += new System.EventHandler(this.btnDecimal_Click);
+            this.btnDecimal.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // btn0
             // 
@@ -273,7 +273,7 @@
             this.btn0.TabIndex = 17;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
-            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            this.btn0.Click += new System.EventHandler(this.NumericBtnClick);
             // 
             // txtfield
             // 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace simple_calculator
@@ -13,83 +8,43 @@ namespace simple_calculator
     public partial class Form1 : Form
     {
         public Form1() => InitializeComponent();
-        
         #region Events
-        void btn1_Click(object sender, EventArgs e) => UpdateTextField("1");
-        void btn2_Click(object sender, EventArgs e) => UpdateTextField("2");
-        void btn3_Click(object sender, EventArgs e) => UpdateTextField("3");
-        void btn4_Click(object sender, EventArgs e) => UpdateTextField("4");
-        void btn5_Click(object sender, EventArgs e) => UpdateTextField("5");
-        void btn6_Click(object sender, EventArgs e) => UpdateTextField("6");
-        void btn7_Click(object sender, EventArgs e) => UpdateTextField("7");
-        void btn8_Click(object sender, EventArgs e) => UpdateTextField("8");
-        void btn9_Click(object sender, EventArgs e) => UpdateTextField("9");
-        void btn0_Click(object sender, EventArgs e) => UpdateTextField("0");
-        void btnDecimal_Click(object sender, EventArgs e) => UpdateTextField(".");
-        void btnPlus_Click(object sender, EventArgs e) => Operate('+');
-        void btnMinus_Click(object sender, EventArgs e) => Operate('-');
-        void btnMultiply_Click(object sender, EventArgs e) => Operate('*');
-        void btnDivide_Click(object sender, EventArgs e) => Operate('/');
+        void NumericBtnClick(object sender, EventArgs e) => UpdateTextField(((Button)sender).Text);            
+        void ArithematicBtnClick(object sender, EventArgs e) => Operate(((Button)sender).Text[0]);
         void btnReset_Click(object sender, EventArgs e) => Reset();               
         #endregion
-
-        
         #region Event Handlers
         void UpdateTextField(string value) => txtfield.Text = $"{txtfield.Text}{value}";
         void Reset() => txtfield.Text = string.Empty;
         void ClearLastChar(string s) => txtfield.Text = s.Length > 0 ? s.Substring(0, s.Length - 1) : txtfield.Text;
         #endregion
-
-
         #region Calculator Logic
         void Operate(char symbol) => txtfield.Text = IsMultiValue() ? Calculate(symbol) : (IsDoubleSymbol(txtfield.Text,txtfield.Text.Length-1) ? txtfield.Text : $"{txtfield.Text}{symbol}");            
-        
         bool IsMultiValue() { return (IsSymbolExist() && txtfield.Text.Split(GetFirstSymbol()).Length > 1) ? true : false; }
-
         bool IsDoubleSymbol(string s, int i) { return (s.Length < 1) ? true : (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/'); }
-        
         bool IsSymbolExist() { return (txtfield.Text.Where<char>(s => s == '+' || s == '-' || s == '*' || s == '/').FirstOrDefault().ToString().Length > 0) ? true : false; }
-
         char GetFirstSymbol() { return txtfield.Text.Where<char>(s => s == '+' || s == '-' || s == '*' || s == '/').FirstOrDefault(); }
-        
         char GetLastSymbol() { return txtfield.Text.Where<char>(s => s == '+' || s == '-' || s == '*' || s == '/').Last(); }
-
         string Calculate(char symbol)
         {
-            if (IsDoubleSymbol(txtfield.Text, txtfield.Text.Length - 1))
-                return txtfield.Text;
-
-            var val = Split(txtfield.Text,symbol);
-
+            if (IsDoubleSymbol(txtfield.Text, txtfield.Text.Length - 1)) { return txtfield.Text; }
+            var _ = Split(txtfield.Text);
             switch (GetFirstSymbol())
             {
-                case '+': return (val.v0 + val.v1).ToString("F1") + symbol;
-                case '-': return (val.v0 - val.v1).ToString("F1") + symbol;
-                case '*': return (val.v0 * val.v1).ToString("F1") + symbol;
-                case '/': return (val.v0 / val.v1).ToString("F1") + symbol;
+                case '+': return (_.v0 + _.v1).ToString("F1") + symbol;
+                case '-': return (_.v0 - _.v1).ToString("F1") + symbol;
+                case '*': return (_.v0 * _.v1).ToString("F1") + symbol;
+                case '/': return (_.v0 / _.v1).ToString("F1") + symbol;
+                default: return txtfield.Text;
             }
-            return symbol.ToString();
         }
-
-        (float v0, float v1) Split(string exp,char symbol)
+        (float v0, float v1) Split(string exp)
         {
-            bool isNegative = false;
-
-            if (exp[0] == '-')
-            {
-                isNegative = true;
-                exp = exp.Substring(1);
-            }
-
-            var vals = exp.Split(GetLastSymbol());
-
-            float v0 = isNegative ? float.Parse(vals[0]) * (-1) : float.Parse(vals[0]); 
-            float v1=float.Parse(vals[1]);
-
-            return (v0,v1);
+            exp = (exp[0] == '-') ? exp.Substring(1) : exp;
+            bool isNegative = (txtfield.Text.Length > exp.Length) ? true : false;
+            var _ = exp.Split(GetLastSymbol());
+            return (isNegative ? float.Parse(_[0]) * (-1) : float.Parse(_[0]), float.Parse(_[1]));
         }
         #endregion
-
-
     }
 }
